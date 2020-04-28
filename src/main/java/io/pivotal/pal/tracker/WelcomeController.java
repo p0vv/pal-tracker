0c85553019faf5c3f3ad063b8d39c72c7f0a8f66
Ubuntu <ubuntu@ip-172-31-65-61.ec2.internal>
2020-04-28 18:22:09 +0000

    Lab 2 Configs annotation wildcard

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,7 +2,7 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.*;
 
 @RestController
 public class WelcomeController {
